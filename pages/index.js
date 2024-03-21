aef21ceedac80acc07961c62f464213dd7dea303
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2024-03-21 10:48:07 +0700

    fix error when import Footer

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import React from "react";
+import Footer from "../components/Footer/Footer";
 
 const index = () => {
-  return <div>hehe</div>;
+  return <>
+    <div>hehe check</div>
+    <Footer/>
+  </>
 };
 
 export default index;
