32b0c5053277edd3fdb1bec5212d8375ce2f13d8
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2024-04-16 08:39:01 +0700

    handle element of homepage

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -2,5 +2,10 @@ import Button from "./Button/Button";
 import Footer from "./Footer/Footer";
 import NavBar from "./Navbar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
+import Service from "./Service/Service";
+import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
+import Subscribe from "./Subscribe/Subscribe";
+import Title from "./Title/Title";
+import Category from "./Category/Category";
 
-export { Button, Footer, NavBar, HeroSection};
+export { Button, Footer, NavBar, HeroSection, Service, BigNFTSlider, Subscribe, Title, Category};
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Style from "../styles/index.module.css";
-import { HeroSection } from "../components/componentsindex";
+import { HeroSection, Service, BigNFTSlider, Subscribe, Title, Category} from "../components/componentsindex";
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
+      <Service />
+      <BigNFTSlider />
+      <Title
+        heading="Audio Collection"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <Title
+        heading="Browse by category"
+        paragraph="Explore the NFTs in the most featured categories."
+      />
+      <Category />
+      <Subscribe />
     </div>
   );
 };
